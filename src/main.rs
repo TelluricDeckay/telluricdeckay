5a1e61dd5ad819dd7bdd17d379d31ca71e086c10
andy5995 <andy400-dev@yahoo.com>
2020-08-15 22:32:35 -0500

    handle folding and checking
    Bambi adds 1 for the ante
    Randi adds 1 for the ante
    Candi adds 1 for the ante
    Paul adds 1 for the ante
    John adds 1 for the ante
    Bambi checks.
    Randi checks.
    Candi opens with 5
    Paul calls bet of $5
    and raises $8
    John calls bet of $13
    Bambi calls bet of $13
    and raises $8
    Randi Folds.
    Candi calls bet of $16
    and raises $8
    Paul calls bet of $16
    and raises $8
    John Folds.
    Bambi calls bet of $16
    Candi calls bet of $8
    Total in pot = $129
    Player Bambi got a Nothing and has 62 chips remaining
    Player Randi got a Nothing and has 99 chips remaining
    Player Candi got a Nothing and has 62 chips remaining
    Player Paul got a Straight and has 62 chips remaining
    Player John got a Pair and has 86 chips remaining

--- src/main.rs
+++ src/main.rs
@@ -37,14 +37,20 @@ fn main() -> Result<(), io::Error> {
     }
     println!("Player Nick is {}", config_data.player_nick);
 
-    for _test_games in 0..9 {
+    for _test_games in 0..4 {
         let mut new_game = {
             // The server will keep track of connected players.
             //
             // Connected players will select if they will be joining a pending game
 
             // FIXME: Players should not start with the same amount of chips for every game.
-            let players = vec![player::Player::new("P1"), player::Player::new("P2")];
+            let players = vec![
+                player::Player::new("Bambi"),
+                player::Player::new("Randi"),
+                player::Player::new("Candi"),
+                player::Player::new("Paul"),
+                player::Player::new("John"),
+            ];
 
             // The host will select to start the game
             game::Game {
@@ -93,15 +99,21 @@ fn main() -> Result<(), io::Error> {
                 // println!();
                 new_game.turns_this_round += 1;
 
-                if (pl.total_amount_added_this_round == new_game.initial_bet_plus_raises)
-                    && (new_game.turns_this_round > number_of_players)
+                if pl.has_folded {
+                    continue;
+                }
+
+                if (pl.total_amount_added_this_round == new_game.initial_bet_plus_raises
+                    && new_game.turns_this_round > number_of_players
+                    || new_game.turns_this_round == number_of_players
+                        && new_game.initial_bet_plus_raises == 0)
                 {
                     new_game.all_bets_paid = true;
                     break;
                 }
                 match new_game.previous_player {
                     None => {
-                        input = player::Action::Open;
+                        input = player::Action::Check;
                         new_game.previous_player = Some(*pl);
 
                         // The proper conversions will need to be done for this to work
@@ -134,24 +146,64 @@ fn main() -> Result<(), io::Error> {
                                 println!("{} opens with {}", pl.name, input_open);
                                 new_game.initial_bet_plus_raises = input_open;
                             }
-                            player::Action::Fold => new_game.previous_player_action = input,
+                            player::Action::Fold => pl.has_folded = player::fold(&pl.name),
+
                             // This player should now be allowed to still "observe" the game
                             // until the end. They don't have to show their cards.
                             //
-                            player::Action::Check => new_game.previous_player_action = input,
-                            _ => println!("Condition mismatch 1"), // The UI should only allow the options above
+                            player::Action::Check => {
+                                new_game.previous_player_action = input;
+                                println!("{} checks.", pl.name);
+                            }
+                            _ => println!("!Condition mismatch 1"), // The UI should only allow the options above
                         }
                     }
                     _ => {
-                        if new_game.turns_this_round >= 4 {
-                            input = player::Action::Call;
-                        } else {
-                            input = player::Action::Raise;
-                        }
+                        match new_game.initial_bet_plus_raises == 0 {
+                            true => {
+                                match new_game.turns_this_round {
+                                    2 => input = player::Action::Check,
+                                    _ => input = player::Action::Open,
+                                }
+                                match input {
+                                    player::Action::Open => {
+                                        new_game.previous_player_action = input;
+                                        let input_open = 5;
+                                        player::open(
+                                            input_open,
+                                            &mut pl.chips,
+                                            &mut pl.total_amount_added_this_round,
+                                            &mut new_game.initial_bet_plus_raises,
+                                            &mut new_game.pot,
+                                        );
+                                        println!("{} opens with {}", pl.name, input_open);
+                                        new_game.initial_bet_plus_raises = input_open;
+                                    }
+                                    player::Action::Fold => {
+                                        pl.has_folded = player::fold(&pl.name);
+                                    }
+                                    // This player should now be allowed to still "observe" the game
+                                    // until the end. They don't have to show their cards.
+                                    //
+                                    player::Action::Check => {
+                                        new_game.previous_player_action = input;
+                                        println!("{} checks.", pl.name);
+                                    }
+                                    _ => println!("!Condition mismatch 2"), // The UI should only allow the options above
+                                }
+                            }
 
-                        new_game.previous_player = Some(*pl);
-                        match new_game.previous_player_action {
-                            player::Action::Open => {
+                            false => {
+                                match new_game.turns_this_round {
+                                    4 => input = player::Action::Raise,
+                                    5 => input = player::Action::Call,
+                                    6 => input = player::Action::Raise,
+                                    7 => input = player::Action::Fold,
+                                    8 => input = player::Action::Raise,
+                                    9 => input = player::Action::Raise,
+                                    10 => input = player::Action::Fold,
+                                    _ => input = player::Action::Call,
+                                }
                                 match input {
                                     player::Action::Call => {
                                         new_game.previous_player_action = input;
@@ -166,16 +218,8 @@ fn main() -> Result<(), io::Error> {
                                     player::Action::Raise => {
                                         new_game.previous_player_action = input;
                                         let input_raise = 8;
-                                        // In a "real" game, a player must call first then raise. So we'll
-                                        // use both the call and raise functions here.
-                                        player::call(
-                                            &pl.name,
-                                            &mut pl.chips,
-                                            &mut pl.total_amount_added_this_round,
-                                            &new_game.initial_bet_plus_raises,
-                                            &mut new_game.pot,
-                                        );
                                         player::raise(
+                                            &pl.name,
                                             &input_raise,
                                             &mut pl.chips,
                                             &mut pl.total_amount_added_this_round,
@@ -183,79 +227,33 @@ fn main() -> Result<(), io::Error> {
                                             &mut new_game.pot,
                                         );
                                     }
-                                    player::Action::Check => {
-                                        new_game.previous_player_action = input
-                                    }
-                                    player::Action::Fold => new_game.previous_player_action = input,
-                                    _ => println!("Condition mismatch 2"),
+                                    player::Action::Fold => pl.has_folded = player::fold(&pl.name),
+                                    _ => println!("!Condition mismatch 3"),
                                 }
                             }
-                            player::Action::Call => {
-                                new_game.previous_player_action = input;
-                                player::call(
-                                    &pl.name,
-                                    &mut pl.chips,
-                                    &mut pl.total_amount_added_this_round,
-                                    &new_game.initial_bet_plus_raises,
-                                    &mut new_game.pot,
-                                );
-                            }
-
-                            player::Action::Raise => {
-                                new_game.previous_player_action = input;
-                                let input_raise = 8;
-                                // In a "real" game, a player must call first then raise. So we'll
-                                // use both the call and raise functions here.
-                                player::call(
-                                    &pl.name,
-                                    &mut pl.chips,
-                                    &mut pl.total_amount_added_this_round,
-                                    &new_game.initial_bet_plus_raises,
-                                    &mut new_game.pot,
-                                );
-                                player::raise(
-                                    &input_raise,
-                                    &mut pl.chips,
-                                    &mut pl.total_amount_added_this_round,
-                                    &mut new_game.initial_bet_plus_raises,
-                                    &mut new_game.pot,
-                                );
-                            }
-                            player::Action::Check => new_game.previous_player_action = input,
-                            player::Action::Fold => new_game.previous_player_action = input,
-                            _ => println!("Condition mismatch 3"), // The UI should only allow the options above.
                         }
                     }
                 }
             }
         }
 
+        // TODO: Don't show the hands for players that folded
+
         // Showdown
-
-        if telluric_handeval::poker::evaluate(&mut new_game.players[0].hand)
-            > telluric_handeval::poker::evaluate(&mut new_game.players[1].hand)
-        {
-            new_game.players[0].chips += new_game.pot;
-            println!("{} wins!", new_game.players[0].name);
-        } else if telluric_handeval::poker::evaluate(&mut new_game.players[0].hand)
-            < telluric_handeval::poker::evaluate(&mut new_game.players[1].hand)
-        {
-            new_game.players[1].chips += new_game.pot;
-            println!("{} wins!", new_game.players[1].name);
-        } else {
-            println!("A tie!");
-        }
-
+        println!("Total in pot = ${}", new_game.pot);
         for pl in new_game.players.iter_mut() {
-            println!("Total in pot = ${}", new_game.pot);
             println!(
-                "Player {} got a {} and had {} chips remaining",
+                "Player {} got a {} and has {} chips remaining",
                 pl.name,
                 telluric_handeval::poker::evaluate(&mut pl.hand).name(),
                 pl.chips
             );
         }
         println!();
+
+        // TODO: Hands need to be evaluated for highest rank, and in the event of a tie
+        // compared against each other
+        //
     }
 
     Ok(())
--- src/player.rs
+++ src/player.rs
@@ -27,12 +27,12 @@ pub enum Action {
 pub fn open(
     input_bet: i32,
     chips: &mut i32,
-    total_amount_added_this_round: &mut i32,
+    pl_total_amount_added_this_round: &mut i32,
     initial_bet_plus_raises: &mut i32,
     pot: &mut i32,
 ) {
     *chips -= input_bet;
-    *total_amount_added_this_round += input_bet;
+    *pl_total_amount_added_this_round += input_bet;
     *pot += input_bet;
     *initial_bet_plus_raises += input_bet;
 }
@@ -40,30 +40,47 @@ pub fn open(
 pub fn call(
     name: &str,
     chips: &mut i32,
-    total_amount_added_this_round: &mut i32,
+    pl_total_amount_added_this_round: &mut i32,
     initial_bet_plus_raises: &i32,
     pot: &mut i32,
 ) {
-    let t = initial_bet_plus_raises - *total_amount_added_this_round;
+    let t = initial_bet_plus_raises - *pl_total_amount_added_this_round;
     *chips -= t;
-    *total_amount_added_this_round += t;
+    *pl_total_amount_added_this_round += t;
     *pot += t;
     println!("{} calls bet of ${}", name, t);
 }
 
 pub fn raise(
+    name: &str,
     input_raise: &i32,
     chips: &mut i32,
-    total_amount_added_this_round: &mut i32,
+    pl_total_amount_added_this_round: &mut i32,
     initial_bet_plus_raises: &mut i32,
     pot: &mut i32,
 ) {
+    call(
+        name,
+        chips,
+        pl_total_amount_added_this_round,
+        initial_bet_plus_raises,
+        pot,
+    );
+
     let t = input_raise;
     *chips -= t;
-    *total_amount_added_this_round += t;
+    *pl_total_amount_added_this_round += t;
     *pot += t;
     println!("and raises ${}", input_raise);
     *initial_bet_plus_raises += input_raise;
+}
+
+// Players that fold shouldn't be popped from the 'players' vector;
+// the server and client needs to know about them, and throught the
+// game, and at the end, display that they folded.
+pub fn fold(name: &str) -> bool {
+    println!("{} Folds.", name);
+    true
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -72,6 +89,7 @@ pub struct Player {
     pub hand: [Card; 5],
     pub chips: i32,
     pub total_amount_added_this_round: i32,
+    pub has_folded: bool,
 }
 
 impl Player {
@@ -88,6 +106,7 @@ impl Player {
             ],
             chips: 100,
             total_amount_added_this_round: 0,
+            has_folded: false,
         }
     }
 }
