9e0cad2cec9c047c1206a00985d9971af1708e69
andy5995 <andy400-dev@yahoo.com>
2020-09-02 22:21:17 -0500

    Convert config options to u32

--- src/config.rs
+++ src/config.rs
@@ -12,10 +12,10 @@ pub fn get_filename(opt_cfg: Option<String>) -> String {
 pub struct Data {
     pub player_nick: String,
     pub is_server: bool,
-    pub server_port: i32,
-    pub max_players: String,
-    pub max_bet: String,
-    pub max_raises: String,
+    pub server_port: u32,
+    pub max_players: u32,
+    pub max_bet: u32,
+    pub max_raises: u32,
 }
 
 impl Data {
@@ -24,9 +24,9 @@ impl Data {
             player_nick: String::new(),
             is_server: false,
             server_port: 0,
-            max_players: String::new(),
-            max_bet: String::new(),
-            max_raises: String::new(),
+            max_players: 0,
+            max_bet: 0,
+            max_raises: 0,
         }
     }
 }
@@ -52,10 +52,11 @@ pub fn get() -> Data {
     for i in &config_vec {
         match i.option.as_ref() {
             "PlayerNick" => config_data.player_nick = i.value.primary.clone(),
+            "Server.port" => config_data.server_port = i.value.primary.parse().expect("Invalid port number"),
             "testing.interactive" => testing_interactive = true,
-            "max.players" => config_data.max_players = i.value.primary.clone(),
-            "max.bet" => config_data.max_bet = i.value.primary.clone(),
-            "max.raises" => config_data.max_raises = i.value.primary.clone(),
+            "max.players" => config_data.max_players = i.value.primary.parse().expect("Invalid max players specified"),
+            "max.bet" => config_data.max_bet = i.value.primary.parse().expect("Invalid max bet specified"),
+            "max.raises" => config_data.max_raises = i.value.primary.parse().expect("Invalid max raises specified"),
 
             // Needs conversion from str to i32
             // "Server.port" => config_data.server_port = i.value.primary.clone(),
