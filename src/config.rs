f64b58a91c17ac25a986d5c00f03b8577271b164
andy5995 <andy400-dev@yahoo.com>
2020-11-08 02:07:56 -0600

    use config file from home, create if it doesn't exist

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,69 @@
+use std::{
+    env, fs,
+    io::{self, ErrorKind, Write},
+    path::Path,
+};
 use structopt::StructOpt;
-use telluricdeckay::cli_options;
-use telluricdeckay::config_h;
+use telluricdeckay::{cli_options, config_h};
 
+pub fn get_homedir() -> io::Result<String> {
+    let homedir: String = match dirs::home_dir() {
+        Some(homedir) => homedir.to_str().unwrap().into(),
+        None => {
+            return Err(io::Error::new(
+                ErrorKind::NotFound,
+                "Unable to determine homedir",
+            ))
+        }
+    };
+    Ok(homedir)
+}
+
+fn write_config(f: &str) -> std::io::Result<()> {
+    let mut w = Vec::new();
+    writeln!(&mut w, "PlayerNick = New Player")?;
+    writeln!(&mut w, "Server.true")?;
+    writeln!(&mut w, "Server.port = 61357")?;
+    writeln!(&mut w, "max.players = 5")?;
+    writeln!(&mut w, "max.bet = 50")?;
+    writeln!(&mut w, "max.raises = 3")?;
+
+    fs::write(&f, &w)
+}
+
+// Get the path and filename to the config file.
+// If it wasn't given as a cli argument,
+// first check for its existence in the current directory; if not found there,
+// check to see if $XDG_CONFIG_HOME is set, if not, use homedir.
 pub fn get_filename(opt_cfg: Option<String>) -> String {
     if opt_cfg.is_none() {
-        return "./telluricdeckayrc".to_string();
+        let basename = "telluricdeckayrc".to_owned();
+        let cwd = "./".to_owned();
+        let default = cwd + &basename;
+        if std::path::Path::new(&default).exists() {
+            return default;
+        }
+        let config_home = match env::var("XDG_CONFIG_HOME") {
+            Ok(val) => val,
+            Err(_e) => get_homedir().unwrap() + "/.config",
+        };
+        if !Path::new(&config_home).exists() {
+            println!("Creating {}", &config_home);
+            fs::create_dir_all(&config_home).expect("Unable to create config directory");
+        }
+
+        let config_file = config_home + "/" + &basename;
+        if !std::path::Path::new(&config_file).exists() {
+            write_config(&config_file).expect("Error writing config file");
+        }
+
+        return config_file;
     }
     opt_cfg.unwrap()
 }
+
+// I think the comment below is outdated. Is there any good reason that
+// should be changed to an enum? If not, please remove the comment.
 
 // TODO change to enum and add stringification
 pub struct Data {
