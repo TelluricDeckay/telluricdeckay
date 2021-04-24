a81b5a2c504a90dcf95cb52f9eb2e9c4271fc8a9
Jammyjamjamman <jdsherratt3@gmail.com>
2021-04-24 22:00:31 +0100

    Switch to png
    * Stop using SVGs, because the UI was too unstable.
    * Improve the UI layout.

--- src/gui/asset_manager.rs
+++ src/gui/asset_manager.rs
@@ -1,241 +1,240 @@
 use crate::config;
-use iced::{window::Icon, Length, Svg};
+use iced::{window::Icon, Length, Image};
 use image::{io::Reader as ImageReader, ImageResult, RgbaImage};
 use ionic_deckhandler::{Card, Rank, Suit};
 
 // TODO: these assets can take time to load. Perhaps look at way to cache them.
 // Card display.
-fn card_img(card_name: &str) -> Svg {
-    Svg::from_path(format!("{}/{}", config::get_cardsdir(), card_name))
-        .width(Length::Units(50))
-        .height(Length::Units(50))
+fn card_img(card_name: &str) -> Image {
+    Image::new(format!("{}/{}", config::get_cardsdir(), card_name))
+        .width(Length::Units(60))
 }
 
 trait CardToImg {
-    fn get_card_img(&self) -> Svg;
+    fn get_card_img(&self) -> Image;
 }
 
 impl CardToImg for Card {
-    fn get_card_img(&self) -> Svg {
+    fn get_card_img(&self) -> Image {
         match *self {
             Card {
                 rank: Rank::Ace,
                 suit: Suit::Spades,
-            } => card_img("AS.svg"),
+            } => card_img("AS.png"),
             Card {
                 rank: Rank::King,
                 suit: Suit::Spades,
-            } => card_img("KS.svg"),
+            } => card_img("KS.png"),
             Card {
                 rank: Rank::Queen,
                 suit: Suit::Spades,
-            } => card_img("QS.svg"),
+            } => card_img("QS.png"),
             Card {
                 rank: Rank::Jack,
                 suit: Suit::Spades,
-            } => card_img("JS.svg"),
+            } => card_img("JS.png"),
             Card {
                 rank: Rank::Ten,
                 suit: Suit::Spades,
-            } => card_img("TS.svg"),
+            } => card_img("TS.png"),
             Card {
                 rank: Rank::Nine,
                 suit: Suit::Spades,
-            } => card_img("9S.svg"),
+            } => card_img("9S.png"),
             Card {
                 rank: Rank::Eight,
                 suit: Suit::Spades,
-            } => card_img("8S.svg"),
+            } => card_img("8S.png"),
             Card {
                 rank: Rank::Seven,
                 suit: Suit::Spades,
-            } => card_img("7S.svg"),
+            } => card_img("7S.png"),
             Card {
                 rank: Rank::Six,
                 suit: Suit::Spades,
-            } => card_img("6S.svg"),
+            } => card_img("6S.png"),
             Card {
                 rank: Rank::Five,
                 suit: Suit::Spades,
-            } => card_img("5S.svg"),
+            } => card_img("5S.png"),
             Card {
                 rank: Rank::Four,
                 suit: Suit::Spades,
-            } => card_img("4S.svg"),
+            } => card_img("4S.png"),
             Card {
                 rank: Rank::Three,
                 suit: Suit::Spades,
-            } => card_img("3S.svg"),
+            } => card_img("3S.png"),
             Card {
                 rank: Rank::Two,
                 suit: Suit::Spades,
-            } => card_img("2S.svg"),
+            } => card_img("2S.png"),
             Card {
                 rank: Rank::Ace,
                 suit: Suit::Hearts,
-            } => card_img("AH.svg"),
+            } => card_img("AH.png"),
             Card {
                 rank: Rank::King,
                 suit: Suit::Hearts,
-            } => card_img("KH.svg"),
+            } => card_img("KH.png"),
             Card {
                 rank: Rank::Queen,
                 suit: Suit::Hearts,
-            } => card_img("QH.svg"),
+            } => card_img("QH.png"),
             Card {
                 rank: Rank::Jack,
                 suit: Suit::Hearts,
-            } => card_img("JH.svg"),
+            } => card_img("JH.png"),
             Card {
                 rank: Rank::Ten,
                 suit: Suit::Hearts,
-            } => card_img("TH.svg"),
+            } => card_img("TH.png"),
             Card {
                 rank: Rank::Nine,
                 suit: Suit::Hearts,
-            } => card_img("9H.svg"),
+            } => card_img("9H.png"),
             Card {
                 rank: Rank::Eight,
                 suit: Suit::Hearts,
-            } => card_img("8H.svg"),
+            } => card_img("8H.png"),
             Card {
                 rank: Rank::Seven,
                 suit: Suit::Hearts,
-            } => card_img("7H.svg"),
+            } => card_img("7H.png"),
             Card {
                 rank: Rank::Six,
                 suit: Suit::Hearts,
-            } => card_img("6H.svg"),
+            } => card_img("6H.png"),
             Card {
                 rank: Rank::Five,
                 suit: Suit::Hearts,
-            } => card_img("5H.svg"),
+            } => card_img("5H.png"),
             Card {
                 rank: Rank::Four,
                 suit: Suit::Hearts,
-            } => card_img("4H.svg"),
+            } => card_img("4H.png"),
             Card {
                 rank: Rank::Three,
                 suit: Suit::Hearts,
-            } => card_img("3H.svg"),
+            } => card_img("3H.png"),
             Card {
                 rank: Rank::Two,
                 suit: Suit::Hearts,
-            } => card_img("2H.svg"),
+            } => card_img("2H.png"),
             Card {
                 rank: Rank::Ace,
                 suit: Suit::Clubs,
-            } => card_img("AC.svg"),
+            } => card_img("AC.png"),
             Card {
                 rank: Rank::King,
                 suit: Suit::Clubs,
-            } => card_img("KC.svg"),
+            } => card_img("KC.png"),
             Card {
                 rank: Rank::Queen,
                 suit: Suit::Clubs,
-            } => card_img("QC.svg"),
+            } => card_img("QC.png"),
             Card {
                 rank: Rank::Jack,
                 suit: Suit::Clubs,
-            } => card_img("JC.svg"),
+            } => card_img("JC.png"),
             Card {
                 rank: Rank::Ten,
                 suit: Suit::Clubs,
-            } => card_img("TC.svg"),
+            } => card_img("TC.png"),
             Card {
                 rank: Rank::Nine,
                 suit: Suit::Clubs,
-            } => card_img("9C.svg"),
+            } => card_img("9C.png"),
             Card {
                 rank: Rank::Eight,
                 suit: Suit::Clubs,
-            } => card_img("8C.svg"),
+            } => card_img("8C.png"),
             Card {
                 rank: Rank::Seven,
                 suit: Suit::Clubs,
-            } => card_img("7C.svg"),
+            } => card_img("7C.png"),
             Card {
                 rank: Rank::Six,
                 suit: Suit::Clubs,
-            } => card_img("6C.svg"),
+            } => card_img("6C.png"),
             Card {
                 rank: Rank::Five,
                 suit: Suit::Clubs,
-            } => card_img("5C.svg"),
+            } => card_img("5C.png"),
             Card {
                 rank: Rank::Four,
                 suit: Suit::Clubs,
-            } => card_img("4C.svg"),
+            } => card_img("4C.png"),
             Card {
                 rank: Rank::Three,
                 suit: Suit::Clubs,
-            } => card_img("3C.svg"),
+            } => card_img("3C.png"),
             Card {
                 rank: Rank::Two,
                 suit: Suit::Clubs,
-            } => card_img("2C.svg"),
+            } => card_img("2C.png"),
             Card {
                 rank: Rank::Ace,
                 suit: Suit::Diamonds,
-            } => card_img("AD.svg"),
+            } => card_img("AD.png"),
             Card {
                 rank: Rank::King,
                 suit: Suit::Diamonds,
-            } => card_img("KD.svg"),
+            } => card_img("KD.png"),
             Card {
                 rank: Rank::Queen,
                 suit: Suit::Diamonds,
-            } => card_img("QD.svg"),
+            } => card_img("QD.png"),
             Card {
                 rank: Rank::Jack,
                 suit: Suit::Diamonds,
-            } => card_img("JD.svg"),
+            } => card_img("JD.png"),
             Card {
                 rank: Rank::Ten,
                 suit: Suit::Diamonds,
-            } => card_img("TD.svg"),
+            } => card_img("TD.png"),
             Card {
                 rank: Rank::Nine,
                 suit: Suit::Diamonds,
-            } => card_img("9D.svg"),
+            } => card_img("9D.png"),
             Card {
                 rank: Rank::Eight,
                 suit: Suit::Diamonds,
-            } => card_img("8D.svg"),
+            } => card_img("8D.png"),
             Card {
                 rank: Rank::Seven,
                 suit: Suit::Diamonds,
-            } => card_img("7D.svg"),
+            } => card_img("7D.png"),
             Card {
                 rank: Rank::Six,
                 suit: Suit::Diamonds,
-            } => card_img("6D.svg"),
+            } => card_img("6D.png"),
             Card {
                 rank: Rank::Five,
                 suit: Suit::Diamonds,
-            } => card_img("5D.svg"),
+            } => card_img("5D.png"),
             Card {
                 rank: Rank::Four,
                 suit: Suit::Diamonds,
-            } => card_img("4D.svg"),
+            } => card_img("4D.png"),
             Card {
                 rank: Rank::Three,
                 suit: Suit::Diamonds,
-            } => card_img("3D.svg"),
+            } => card_img("3D.png"),
             Card {
                 rank: Rank::Two,
                 suit: Suit::Diamonds,
-            } => card_img("2D.svg"),
+            } => card_img("2D.png"),
         }
     }
 }
 
 pub(in crate::gui) trait HandToImgs {
-    fn get_hand_imgs(&self) -> Vec<Svg>;
+    fn get_hand_imgs(&self) -> Vec<Image>;
 }
 
 impl HandToImgs for [Card] {
-    fn get_hand_imgs(&self) -> Vec<Svg> {
+    fn get_hand_imgs(&self) -> Vec<Image> {
         self.iter().map(|c| c.get_card_img()).collect()
     }
 }
